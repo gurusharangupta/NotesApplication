bf626238881f63768c02bad275bd990760f7120f
gurusharangupta <gupta.gurusharan@gmail.com>
2016-05-24 14:45:56 +0530

    Update AppllcationConstant.java

--- src/com/got/constant/AppllcationConstant.java
+++ src/com/got/constant/AppllcationConstant.java
@@ -6,7 +6,7 @@ public interface AppllcationConstant {
 	public static final String NOTE_SAVE_SUCCESSFUL = "NOTE SAVE WAS SUCCESSFUL";
 	public static final String NOTE_SAVE_UNSUCCESSFUL = "NOTE SAVE WAS NOT SUCCESSFUL";
 	public static final String USER_NOT_FOUND = "Please verify Username and Password";
-	public static final String NOTE_NOT_FOUND = "Note was not Found for this user";
+	public static final String NOTE_NOT_FOUND = "Notes was not Found for this user";
 	public static final String AUTHORIZAITON_HEADER_KEY = "Authorization";
 	public static final String AUTHORIZATION_HEADER_PREFIX ="Basic ";
 	public static final String NOTE_UPDATE_SUCCESSFUL ="Note was updated";
